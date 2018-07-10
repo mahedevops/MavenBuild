d3f3013dfb26632fe7c1539df26b03084dfb1742
maheshdevops <s.nandigama1@gmail.com>
2018-07-10 14:12:35 -0400

    updated

--- src/main/java/com/geekcap/vmturbo/Thing.java
+++ src/main/java/com/geekcap/vmturbo/Thing.java
@@ -3,11 +3,11 @@ package com.geekcap.vmturbo;
 public class Thing {
   private int n = 0;
 
-  public int getN() {
+  public int final getN() {
     return n;
   }
 
-  public void setN(int n) {
+  public void final setN(int final n) {
     this.n = n;
   }
 }
